92655aad5b32c3b8fb4357f2ed460e8e79b4dc0b
abeldoescoding <abeldoescoding@gmail.com>
2024-01-19 23:35:56 -0500

    create header buttons and script to change color back an forth on hover

---
+++ script.js
@@ -0,0 +1,31 @@
+const bioButton = document.querySelector("#bio-button");
+bioButton.addEventListener("mouseover", () => {
+    bioButton.style.background = '#B02851';
+})
+bioButton.addEventListener("mouseout", () => {
+    bioButton.style.background = 'rgb(193, 167, 203)';
+})
+
+const musicButton = document.querySelector("#music-button");
+musicButton.addEventListener("mouseover", () => {
+    musicButton.style.background = '#B02851';
+})
+musicButton.addEventListener("mouseout", () => {
+    musicButton.style.background = 'rgb(193, 167, 203)';
+})
+
+const showsButton = document.querySelector("#shows-button");
+showsButton.addEventListener("mouseover", () => {
+    showsButton.style.background = '#B02851';
+})
+showsButton.addEventListener("mouseout", () => {
+    showsButton.style.background = 'rgb(193, 167, 203)';
+})
+
+const merchButton = document.querySelector("#merch-button");
+merchButton.addEventListener("mouseover", () => {
+    merchButton.style.background = '#B02851';
+})
+merchButton.addEventListener("mouseout", () => {
+    merchButton.style.background = 'rgb(193, 167, 203)';
+})
