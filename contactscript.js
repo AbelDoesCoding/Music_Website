d563f252bc3394b99102186ef04c99091e80c714
abeldoescoding <abeldoescoding@gmail.com>
2024-01-20 01:49:05 -0500

    added function to contact form to make sure email address is correct. need to add more going forward

--- contactscript.js
+++ contactscript.js
@@ -1,11 +1,3 @@
-const contactButton = document.querySelector("#contact-button");
-contactButton.addEventListener("mouseover", () => {
-    contactButton.style.background = '#B02851';
-});
-contactButton.addEventListener("mouseout", () => {
-    contactButton.style.background = 'rgb(193, 167, 203)';
-});
-
 const musicButton = document.querySelector("#music-button");
 musicButton.addEventListener("mouseover", () => {
     musicButton.style.background = '#B02851';
@@ -29,4 +21,29 @@ merchButton.addEventListener("mouseover", () => {
 merchButton.addEventListener("mouseout", () => {
     merchButton.style.background = 'rgb(193, 167, 203)';
 });
+
+const homeButton = document.querySelector("#home-button");
+homeButton.addEventListener("mouseover", () => {
+    homeButton.style.background = '#B02851';
+});
+homeButton.addEventListener("mouseout", () => {
+    homeButton.style.background = 'rgb(193, 167, 203)';
+});
+
+// Form submit functions
+var emailCorrect = false;
+
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener("click", () => {
+    var email = document.getElementById("email").value;
+    for (let i = 0; i < email.length; i++) {
+        if (email[i] == "@") {
+            emailCorrect = true;
+        }
+    }
+    if (emailCorrect == false) {
+        alert("You might wanna recheck that email address, friend.");
+        // eventually use this part of function to block sending form info if false
+    }
+})
 
