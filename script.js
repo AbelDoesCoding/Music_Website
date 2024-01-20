717a9818488a778205f923075579819509088cd0
abeldoescoding <abeldoescoding@gmail.com>
2024-01-20 01:14:18 -0500

    created structure for contact page

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 const contactButton = document.querySelector("#contact-button");
 contactButton.addEventListener("mouseover", () => {
     contactButton.style.background = '#B02851';
-})
+});
 contactButton.addEventListener("mouseout", () => {
     contactButton.style.background = 'rgb(193, 167, 203)';
-})
+});
 
 const musicButton = document.querySelector("#music-button");
 musicButton.addEventListener("mouseover", () => {
     musicButton.style.background = '#B02851';
-})
+});
 musicButton.addEventListener("mouseout", () => {
     musicButton.style.background = 'rgb(193, 167, 203)';
-})
+});
 
 const showsButton = document.querySelector("#shows-button");
 showsButton.addEventListener("mouseover", () => {
     showsButton.style.background = '#B02851';
-})
+});
 showsButton.addEventListener("mouseout", () => {
     showsButton.style.background = 'rgb(193, 167, 203)';
-})
+});
 
 const merchButton = document.querySelector("#merch-button");
 merchButton.addEventListener("mouseover", () => {
     merchButton.style.background = '#B02851';
-})
+});
 merchButton.addEventListener("mouseout", () => {
     merchButton.style.background = 'rgb(193, 167, 203)';
-})
+});
+
+const homeButton = document.querySelector("#home-button");
+homeButton.addEventListener("mouseover", () => {
+    homeButton.style.background = '#B02851';
+});
+homeButton.addEventListener("mouseout", () => {
+    homeButton.style.background = 'rgb(193, 167, 203)';
+});
